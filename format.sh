4a13fd8754ae1153c87a37675d085d0c64d10832
Woojin Kim <woojinkim@unist.ac.kr>
2024-09-10 14:57:59 +0900

    Delete unnecessaries

--- format.sh
+++
@@ -1,6 +0,0 @@
-all_files=$(git ls-tree --full-tree -r --name-only HEAD .)
-py_files=$(echo "$all_files" | grep ".*\.py$")
-nb_files=$(echo "$all_files" | grep ".*\.ipynb$" | grep -v "^notebooks")
-python -m black $py_files $nb_files
-python -m isort $py_files $nb_files
-python -m flake8 $py_files
